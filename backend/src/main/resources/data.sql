40dcff220249bc6ddd4bcc1c8ffbbf327edef25b
rmsals8 <rmsals8@naver.com>
2025-03-11 16:38:31 +0900

    mybatis- 로그인 ,jpa - 로그 구현

--- backend/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
--- 테스트 사용자 데이터
-INSERT INTO users (username, password, email, nickname) 
-VALUES ('testuser', '$2a$10$hPmDSuEEFP.u9PiRpgThVOriNh/iRvKwJjmxj7.tPFcvXNI7Hg9lu', 'test@example.com', '테스트유저')
-ON DUPLICATE KEY UPDATE username = VALUES(username);
--- 비밀번호: test123
